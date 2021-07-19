4511739173a59d4f8883347a9360a6191713af75
shenli <shenli@tsign.cn>
2021-07-20 07:50:01 +0800

    code

---
+++ src/main/java/com/algorithm/leetcode/array/BuyStocks.java
@@ -0,0 +1,125 @@
+package com.algorithm.leetcode.array;
+
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * <p>
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * <p>
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+public class BuyStocks {
+    /**
+     * 1 <= prices.length <= 105
+     * 0 <= prices[i] <= 104
+     * soluation 1 brute force
+     * 暴力解法：如果有时间和空间复杂度的限制，这种解法不可取
+     * Time complexity o(n²)
+     */
+
+    public static int maxProfitSolutionOne(int[] prices) {
+        int buyIn = 0;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            buyIn = prices[i];
+            for (int j = i; j < prices.length; j++) {
+                max = Math.max(max, prices[j] - buyIn);
+            }
+        }
+        if (max > 0) {
+            return max;
+        }
+        return 0;
+
+    }
+
+    //solution 2  DP
+    public static int maxProfitSolutionTwo(int[] prices) {
+        int n = prices.length;
+        if (n < 1) return 0;
+        int[] minPrices = new int[n];
+        int[] maxProfit = new int[n];
+        minPrices[0] = prices[0];
+        maxProfit[0] = 0;
+
+        for (int i = 1; i < n; ++i) {
+            minPrices[i] = Math.min(minPrices[i - 1], prices[i]);
+            maxProfit[i] = Math.max(maxProfit[i - 1], prices[i] - minPrices[i - 1]);
+        }
+        return maxProfit[n - 1];
+
+    }
+
+    public static int maxProfitSolutionTwoOptimize(int[] prices) {
+        int n = prices.length;
+        if (n < 1) return 0;
+        int[] minPrices = new int[n];
+        int[] maxProfit = new int[n];
+        minPrices[0] = prices[0];
+        maxProfit[0] = 0;
+
+        for (int i = 1; i < n; ++i) {
+            minPrices[i] = Math.min(minPrices[i - 1], prices[i]);
+            maxProfit[i] = Math.max(maxProfit[i - 1], prices[i] - minPrices[i - 1]);
+        }
+        return maxProfit[n - 1];
+
+    }
+
+    /**
+     * solution3
+     * price: {7,1,5,3,6,4}
+     * gain {-6, 4, -2, 3,-2}
+     * reduce to Maximum subarray
+     * <p>
+     * compute the gain of each day
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfitSolutionThree(int[] prices) {
+        int n = prices.length;
+        int[] gains = new int[n - 1];
+        for (int i = 0; i < prices.length - 1; i++) {
+            gains[i] = prices[i + 1] - prices[i];
+        }
+        return maxiMumSubarray(gains);
+    }
+
+    private static int maxiMumSubarray(int[] array) {
+        int sum = 0;
+        int max =0;
+        for(int j =0; j < array.length; j ++) {
+            if (sum > 0) {
+                sum += array[j];
+            } else {
+                sum = array[j];
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        //test solution one
+        int[] pricesExample1 = new int[]{7, 1, 5, 3, 6, 4};
+        int[] pricesExample2 = new int[]{7, 6, 4, 3, 1};
+        assert maxProfitSolutionOne(pricesExample1) == 5;
+        System.out.println("result " + (maxProfitSolutionOne(pricesExample1) == 5));
+        assert maxProfitSolutionOne(pricesExample2) == 0;
+        System.out.println("result " + (maxProfitSolutionOne(pricesExample2) == 0));
+
+
+        //test solution two
+        assert maxProfitSolutionTwo(pricesExample1) == 5;
+        System.out.println("result " + (maxProfitSolutionTwo(pricesExample1) == 5));
+        assert maxProfitSolutionTwo(pricesExample2) == 0;
+        System.out.println("result " + (maxProfitSolutionTwo(pricesExample2) == 0));
+
+        //test solution three
+        assert maxProfitSolutionThree(pricesExample1) == 5;
+        System.out.println("result " + (maxProfitSolutionThree(pricesExample1) == 5));
+        assert maxProfitSolutionThree(pricesExample2) == 0;
+        System.out.println("result " + (maxProfitSolutionThree(pricesExample2) == 0));
+    }
+}
---
+++ src/main/java/com/algorithm/leetcode/array/TwoSum.java
@@ -0,0 +1,41 @@
+package com.algorithm.leetcode.array;
+import java.util.Arrays;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TwoSum {
+    public static  int[] twoSum(int[] nums, int target) {
+
+        return  new int[]{};
+    }
+
+    public static void main(String[] args) {
+        //test case one
+        int testNums1[] = new int[]{2,7,11,15};
+        int testTarget1 = 9;
+        int result1[] = new int[]{0,1};
+        assert Arrays.equals(twoSum(testNums1, testTarget1) , result1);
+
+        //test case two
+        int testNums2[] = new int[]{3,2,4};
+        int testTarget2 = 6;
+        int result2[] = new int[]{1,2};
+        assert Arrays.equals(twoSum(testNums2, testTarget2) , result2);
+    }
+}
+
