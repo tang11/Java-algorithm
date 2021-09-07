1787170f1009a14aaa8f1fbc02f07d2566731fc0
tanglijuan <shiyi132431@163.com>
2021-09-07 09:08:10 +0800

    pointer

---
+++ src/main/java/com/algorithm/bytype/pointerManipulation/MaxSlidingWindow.java
@@ -0,0 +1,50 @@
+package com.algorithm.bytype.pointerManipulation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author shenli
+ * @date 2021/9/7
+ */
+public class MaxSlidingWindow {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        int start = 0;
+        int end = k - 1;
+        int i = start;
+        int maxValue = Integer.MIN_VALUE;
+        while (i <= end) {
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
+
+            }
+            i++;
+        }
+        list.add(maxValue);
+        start++;
+        end++;
+        while (end < nums.length) {
+            if (nums[end] > maxValue) {
+                maxValue = nums[end];
+            }
+            list.add(maxValue);
+            end++;
+        }
+        int[] res = new int[list.size()];
+        for (int j = 0; j < list.size(); j++) {
+            res[j] = list.get(j);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
+        int[] nums1 = new int[]{1,-1};
+        int k1 =1;
+        System.out.println(Arrays.toString(maxSlidingWindow(nums1,k1)));
+    }
+}
