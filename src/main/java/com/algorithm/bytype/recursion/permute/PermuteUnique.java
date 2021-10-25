9dc06411314fb63dd82aeb142b24cd4802d4f738
tanglijuan <shiyi132431@163.com>
2021-10-25 08:49:12 +0800

    premute

--- src/main/java/com/algorithm/bytype/recursion/permute/PermuteUnique.java
+++ src/main/java/com/algorithm/bytype/recursion/permute/PermuteUnique.java
@@ -11,29 +11,70 @@ import java.util.List;
 public class PermuteUnique {
     public List<List<Integer>> permuteUnique1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack1(res, new ArrayList<>(), nums);
+        boolean[] used = new boolean[nums.length];
+        backtrack1(res, new ArrayList<>(), nums, used, 0);
         return res;
     }
 
-    public void backtrack1(List<List<Integer>> res, List<Integer> tempList, int[] nums) {
+    public void backtrack1(List<List<Integer>> res, List<Integer> tempList, int[] nums, boolean[] used, int index) {
         if (tempList.size() == nums.length) {
             res.add(new ArrayList<>(tempList));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (tempList.contains(i) && i > 0 && nums[i] == nums[i - 1]) {
+            if (used[i]) {
+                continue;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) {
                 continue;
             }
             tempList.add(nums[i]);
-            backtrack1(res, tempList, nums);
+            used[i] = true;
+            backtrack1(res, tempList, nums, used, index);
             tempList.remove(tempList.size() - 1);
+            used[i] = false;
         }
+    }
+
+
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack2(res, new ArrayList<>(), nums, used, 0);
+        return res;
+    }
+
+
+    public void backtrack2(List<List<Integer>> res, List<Integer> tempList, int[] nums, boolean[] used, int index) {
+        if (tempList.size() == nums.length) {
+            res.add(new ArrayList<>(tempList));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            //cut the tree
+            if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+                continue;
+            }
+
+            tempList.add(nums[i]);
+            used[i] = true;
+            backtrack2(res, tempList, nums, used, index);
+            tempList.remove(tempList.size() - 1);
+            used[i] = false;
+        }
+
     }
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 1, 2};
         PermuteUnique permuteUnique = new PermuteUnique();
         System.out.println(permuteUnique.permuteUnique1(nums));
+        System.out.println(permuteUnique.permuteUnique2(nums));
+
     }
 
 }
