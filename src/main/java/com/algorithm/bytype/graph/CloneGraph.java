6dcaa26f0024ad16f82a42df658209895337802c
tanglijuan <shiyi132431@163.com>
2021-10-26 07:16:30 +0800

    graph

--- src/main/java/com/algorithm/bytype/graph/CloneGraph.java
+++ src/main/java/com/algorithm/bytype/graph/CloneGraph.java
@@ -1,15 +1,29 @@
 package com.algorithm.bytype.graph;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * @author tanglijuan
  * @date 2021/8/29
  */
 public class CloneGraph {
+
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
     /**
      * DFS
      * @param node
