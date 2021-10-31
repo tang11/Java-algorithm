00d7f78248a770ee6aa3226d75d210ff1f36b51c
tanglijuan <shiyi132431@163.com>
2021-10-31 13:40:55 +0800

    算法

--- src/main/java/com/algorithm/bytype/hashMap/IsAnagram.java
+++ src/main/java/com/algorithm/bytype/hashMap/IsAnagram.java
@@ -8,10 +8,35 @@ import java.util.Map;
  * @date 2021/8/29
  */
 public class IsAnagram {
-//    public boolean isAnagram(String s, String t) {
-//        Map<Character,Integer> map = new HashMap<>();
-//        for(char c : s.toCharArray()){
-//
-//        }
-//    }
+     static HashMap<Character,Integer> map = new HashMap<>();
+    public static boolean isAnagram(String s, String t) {
+        for(int i =0;i< s.length();i++) {
+            if(map.get(s.charAt(i)) == null) {
+                map.put(s.charAt(i),1);
+            }else {
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+        }
+
+        for(int j =0;j< t.length();j++) {
+            if(map.get(t.charAt(j)) == null) {
+                return false;
+            }else {
+                int num = map.get(t.charAt(j));
+                if(num==1) {
+                    map.remove(t.charAt(j));
+                }else{
+                    map.put(t.charAt(j),num-1);
+                }
+            }
+
+        }
+        return map.size()>0 ? false:true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t= "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
 }
