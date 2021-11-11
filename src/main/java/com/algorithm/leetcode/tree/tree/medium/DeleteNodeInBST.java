2ca81288f9108561718e46ca11837953fbf7dc5c
tanglijuan <shiyi132431@163.com>
2021-11-11 09:36:54 +0800

    recursion

---
+++ src/main/java/com/algorithm/leetcode/tree/tree/medium/DeleteNodeInBST.java
@@ -0,0 +1,25 @@
+package com.algorithm.leetcode.tree.tree.medium;
+
+import com.algorithm.leetcode.tree.TreeNode;
+
+/**
+ * @author shenli
+ * @date 2021/11/11
+ */
+public class DeleteNodeInBST {
+
+    /**
+     * 通用模板
+     * @param root
+     * @param target
+     */
+//    void BST(TreeNode root, int target) {
+//        if (root.val == target)
+//            // 找到目标，做点什么
+//            if (root.val < target)
+//                BST(root.right, target);
+//        if (root.val > target)
+//            BST(root.left, target);
+//    }
+
+}
