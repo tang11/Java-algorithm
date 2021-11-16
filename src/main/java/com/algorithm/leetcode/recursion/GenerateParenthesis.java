702ed15a8911d5c7d7fac4009e81d4931d223465
tanglijuan <shiyi132431@163.com>
2021-11-17 07:57:12 +0800

    recursion

---
+++ src/main/java/com/algorithm/leetcode/recursion/GenerateParenthesis.java
@@ -0,0 +1,8 @@
+package com.algorithm.leetcode.recursion;
+
+/**
+ * @author shenli
+ * @date 2021/11/17
+ */
+public class GenerateParenthesis {
+}
--- src/main/java/com/algorithm/leetcode/recursion/Permute.java
+++ src/main/java/com/algorithm/leetcode/recursion/Permute.java
@@ -1,8 +1,31 @@
 package com.algorithm.leetcode.recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author tanglijuan
  * @date 2021/11/17
  */
 public class Permute {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), nums);
+        return res;
+    }
+
+    void backtrack(List<List<Integer>> res, List<Integer> path, int[] nums) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (path.contains(nums[i])) {
+                continue;
+            }
+            path.add(nums[i]);
+            backtrack(res, path, nums);
+            path.remove(path.size() - 1);
+        }
+    }
 }
