c08f44a46cc001c83e0a2526d38e051884afc2ca
tanglijuan <shiyi132431@163.com>
2021-11-16 09:31:27 +0800

    recursion

--- src/main/java/com/algorithm/leetcode/recursion/CombinationSum.java
+++ src/main/java/com/algorithm/leetcode/recursion/CombinationSum.java
@@ -16,20 +16,21 @@ public class CombinationSum {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(res, new ArrayList<>(), candidates,target,0);
+        backtrack(res, new ArrayList<>(), candidates, target, 0);
         return res;
     }
 
-    void backtrack( List<List<Integer>> res, List<Integer> path,int[] candidates, int target,int begin) {
-        if(target == 0) {
+    void backtrack(List<List<Integer>> res, List<Integer> path, int[] candidates, int target, int begin) {
+        if (target == 0) {
             res.add(new ArrayList<>(path));
             return;
         }
-        for(int i =begin;i< candidates.length;i++) {
-            if(target>0) {
+        for (int i = begin; i < candidates.length; i++) {
+            if (target > 0) {
                 path.add(candidates[i]);
-                backtrack(res,path,candidates,target-candidates[i],i);
-                path.remove(path.size()-1);
+                backtrack(res, path, candidates, target - candidates[i], i);
+                path.remove(path.size() - 1);
+            }
         }
     }
 }
