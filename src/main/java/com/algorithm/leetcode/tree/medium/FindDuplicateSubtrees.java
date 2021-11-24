784889669bda0fee95ad9c464b29a7ed774f77f1
tanglijuan <shiyi132431@163.com>
2021-11-24 08:10:58 +0800

    tree

--- src/main/java/com/algorithm/leetcode/tree/medium/FindDuplicateSubtrees.java
+++ src/main/java/com/algorithm/leetcode/tree/medium/FindDuplicateSubtrees.java
@@ -2,6 +2,9 @@ package com.algorithm.leetcode.tree.medium;
 
 import com.algorithm.leetcode.tree.TreeNode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,12 +12,27 @@ import java.util.List;
  * @date 2021/11/22
  */
 public class FindDuplicateSubtrees {
-    List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+    HashMap<String, Integer> memo = new HashMap<>();
+    LinkedList<TreeNode> res = new LinkedList<>();
 
+    List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        traverse(root);
+        return  res;
     }
 
-    void find(List<>){
-
+    String traverse(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+        String left = traverse(root.left);
+        String right = traverse(root.right);
+        String subTree = left + "," + right + "," + root.val;
+        int freq = memo.getOrDefault(subTree,0);
+        if (freq == 1) {
+            res.add(root);
+        }
+        memo.put(subTree,freq +1);
+        return subTree;
     }
 
 }
