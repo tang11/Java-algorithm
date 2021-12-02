0a199c1f9cee222c6c0f2ae8390f92082b417407
tanglijuan <shiyi132431@163.com>
2021-12-02 08:58:56 +0800

    backtrack

--- src/main/java/com/algorithm/nineType/arecursion/PermutationsII.java
+++ src/main/java/com/algorithm/nineType/arecursion/PermutationsII.java
@@ -11,27 +11,27 @@ public class PermutationsII {
     public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-        backtrack(res, nums, new ArrayList<>(), visited,0);
+        backtrack(res, nums, new ArrayList<>(), visited);
         return res;
 
     }
 
-    public static void backtrack(List<List<Integer>> res, int[] nums, List<Integer> path, boolean[] visited,int level) {
+    public static void backtrack(List<List<Integer>> res, int[] nums, List<Integer> path, boolean[] visited) {
         if (path.size() == nums.length) {
             res.add(new ArrayList<>(path));
-            return;
+            return;L
         }
         for (int i = 0; i < nums.length; i++) {
             if (visited[i]) {
                 continue;
             }
-            if (i!=level && nums[i] == nums[i-1]) {
+            if (i >0 && nums[i] == nums[i - 1] && !visited[i-1]) {
                 continue;
             }
             path.add(nums[i]);
             visited[i] = true;
-            backtrack(res, nums, path, visited,i);
-            visited[i]=false;
+            backtrack(res, nums, path, visited);
+            visited[i] = false;
             path.remove(path.size() - 1);
         }
     }
