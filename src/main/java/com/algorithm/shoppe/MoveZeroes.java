d98ba737d792e19987c40b896771a3fec58e2df0
tanglijuan <shiyi132431@163.com>
2021-12-13 09:54:47 +0800

    firstMissingPositive

---
+++ src/main/java/com/algorithm/shoppe/MoveZeroes.java
@@ -0,0 +1,27 @@
+package com.algorithm.shoppe;
+
+/**
+ * @author shenli
+ * @date 2021/12/13
+ */
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int left =0;
+        int right = 0;
+        while (right<n) {
+            if (nums[right] !=0) {
+                swap(nums,left,right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
